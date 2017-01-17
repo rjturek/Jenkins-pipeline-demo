a87b918b8f07336a482e25be78cbe30cb312fc71
Ambasager Estifanos <anbestephen@gmail.com>
2017-01-17 19:46:45 +0000

    Create a maven Jave Project This project was created from maven-archetype-quickstart archetype

---
+++ Pipeline-Demo/src/main/java/org/anbestephen/App.java
@@ -0,0 +1,13 @@
+package org.anbestephen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
